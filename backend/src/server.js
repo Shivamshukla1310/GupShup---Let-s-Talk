7920707e94fd1e5ff6daf327765702f5379ba70d
Shivamshukla1310 <shukla.shivamv@gmail.com>
2025-11-23 22:00:50 +0530

    Ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
 if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist"))); // here we  are basically taking dist folder under frontend and making it our static asset
 
   app.get("*", (_, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
